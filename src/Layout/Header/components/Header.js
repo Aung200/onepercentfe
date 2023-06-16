51f2f47a982d3e543ced4fdc59353600980ab3c8
aung200 <aung.m.m.phyo@gmail.com>
2023-06-17 02:30:53 +0800

    Commited

--- src/Layout/Header/components/Header.js
+++ src/Layout/Header/components/Header.js
@@ -2,6 +2,7 @@
 import React from 'react'
 //components
 import { Link } from 'react-router-dom';
+import { Nav, Navbar, NavDropdown, Container } from 'react-bootstrap';
 //css
 import './styles/header.css'
 import LOGO from '../../../Assets/Images/onepercentlogo.jpg'
@@ -9,21 +10,37 @@ import LOGO from '../../../Assets/Images/onepercentlogo.jpg'
 function Header() {
     return (
         <header className='header-wrap'>
-            <a href='/'><img className='brand-img' src={LOGO} alt='brand' /></a>
-            <nav>
-              <ul id="navigation">
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                <Link to="/program">About</Link>
-                </li>
-                <li>
-                <Link to="/contact">Contact</Link>
-                </li>
-              </ul>
-            </nav>
-            <p className='brand-description'>ONE PERCENT BETTER EVERYDAY</p>
+            {/* <a href='/'><img className='brand-img' src={LOGO} alt='brand' /></a>
+            <p className='brand-description'>ONE PERCENT BETTER EVERYDAY</p> */}
+            <Navbar collapseOnSelect expand="lg">
+                <Container>
+                    <Navbar.Brand href="/"><img className='brand-img' src={LOGO} alt='brand'/></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Link className="nav-link" to="/program">Program</Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                    Separated link
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">More deets</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                Dank memes
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </header>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { HashRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './App';
+//Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
